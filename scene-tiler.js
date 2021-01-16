6bb3be56f18a32ad38fbf97343d5bda75aaa1db1
zeel <zeel02@gmail.com>
2021-01-16 04:57:51 -0500

    Documented clearScene()

--- scene-tiler.js
+++ scene-tiler.js
@@ -24,6 +24,13 @@ class SceneTiler {
 			await canvas[def.layer].createMany(source.data[def.type]);
 		}
 	}
+
+	/**
+	 * Deletes all placeable objects in the entire scene.
+	 *
+	 * @static
+	 * @memberof SceneTiler
+	 */
 	static async clearScene() {
 		for (const def of Object.values(this.layerDefs)) {
 			await canvas[def.layer].deleteAll();
